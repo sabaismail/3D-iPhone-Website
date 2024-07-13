9ff710930bfc6f4e0f883cb807433b477dd8d53d
sabaismail <sabaismail011@gmail.com>
2024-07-13 20:07:45 +0500

    Features

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://dae859788c727bfa597f097bca557752@o4507594828742656.ingest.de.sentry.io/4507594833002576",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
